073dd7ddf218973acd9ef79e337c1d4dfee46ba3
222 <129908747+613222@users.noreply.github.com>
2024-04-17 21:33:25 -0700

    Update official.py

--- MassUpload/official.py
+++ MassUpload/official.py
@@ -1,3 +1,1 @@
-print("OFFICIAL PACKAGE: https://cdn.613222.dev/ff34fa0afb4647abb6f4f8463d3026e4.zip")
-
-# https://github.com/613222/Roblox/blob/main/MassUpload/official.py
+# IGNORE THIS FILE AS THIS WAS SUPPOSE TO BE A PRANK LOL
